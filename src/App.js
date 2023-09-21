56a25d93e7ba98a85f700756eebf8f6c037f45e8
adicop <adrian.copoiu1@gmail.com>
2023-09-21 14:41:46 +0300

    2 players game implemented + a bit of refactoring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import { useState, useEffect } from 'react';
+import { Grid } from '@mui/material';
 
 import Square from './Components/Square';
 import { WinningPatterns } from './WinningPatterns';
@@ -9,17 +10,20 @@ const initialTable = ["", "", "", "", "", "", "", "", ""];
 
 function App() {
   const [table, setTable] = useState(initialTable);
-  const [player, setPlayer] = useState("X");
+  const [currentPlayer, setCurrentPlayer] = useState({name: null, item: null});
+  const [playerOne, setPlayerOne] = useState({name: null, item: "X"});
+  const [playerTwo, setPlayerTwo] = useState({name: null, item: "O"});
+  const [gameStarted, setGameStarted] = useState(false);
   const [result, setResult] = useState({ winner: null, state: null });
 
   useEffect(() => {
     checkWin();
     checkIfTie();
 
-    if(player === "X") {
-      setPlayer("O");
+    if(currentPlayer.item === "X") {
+      setCurrentPlayer(playerTwo);
     } else {
-      setPlayer("X");
+      setCurrentPlayer(playerOne);
     }
   }, [table]);
 
@@ -27,17 +31,24 @@ function App() {
     const { winner, state } = result;
 
     if (state === "won") {
-      alert(`Game won by ${winner} !!!`);
+      alert(`Game won by: ${winner} !!!`);
       restart();
     } else if (state === "tie") {
-      alert("IT IS A TIE !!!");
+      alert("IT IS A TIE, TRY BETTER NEXT TIME !!!");
       restart();
     }
   }, [result]);
 
+  const initializeGame = () => {
+    const { name: playerOneName } = playerOne;
+    const { name: playerTwoName } = playerTwo;
+
+    playerOneName && playerTwoName ? (setGameStarted(true) && setCurrentPlayer(playerOne)) : alert("PLEASE ENTER YOUR NAMES");
+  }
+
   const onClickSquare = (square) => {
     setTable(table.map((value, index) => {
-      if (index === square && value === "") return player
+      if (index === square && value === "") return currentPlayer.item
 
       return value;
     }))
@@ -58,7 +69,7 @@ function App() {
       })
 
       if(foundWinningPattern) {
-        setResult({ winner: player, state: "won" });
+        setResult({ winner: currentPlayer.name, state: "won" });
       }
     })
   }
@@ -78,29 +89,59 @@ function App() {
 
   const restart = () => {
     setTable(initialTable);
-    setPlayer("X");
   }
 
   return (
-    <div className="App">
-      <div className="table">
-        <div className='row'>
-          <Square value={table[0]} onclick={() => onClickSquare(0)}/>
-          <Square value={table[1]} onclick={() => onClickSquare(1)}/>
-          <Square value={table[2]} onclick={() => onClickSquare(2)}/>
+    <Grid className="wrapper">
+      {!gameStarted ? 
+        <Grid 
+          className="form"
+          container
+          spacing={2}
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item>
+            <input
+              placeholder="Player 1"
+              onBlur={(event) => {
+                setPlayerOne({...playerOne, name: event.target.value});
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <input
+              placeholder="Player 2"
+              onBlur={(event) => {
+                setPlayerTwo({...playerTwo, name: event.target.value});
+              }}
+            />
+          </Grid>
+          <Grid item>
+            <button onClick={initializeGame}>Start Game</button>
+          </Grid>
+        </Grid>
+      : 
+        <div className="table">
+          <div className='row'>
+            <Square value={table[0]} onclick={() => onClickSquare(0)}/>
+            <Square value={table[1]} onclick={() => onClickSquare(1)}/>
+            <Square value={table[2]} onclick={() => onClickSquare(2)}/>
+          </div>
+          <div className='row'>
+            <Square value={table[3]} onclick={() => onClickSquare(3)}/>
+            <Square value={table[4]} onclick={() => onClickSquare(4)}/>
+            <Square value={table[5]} onclick={() => onClickSquare(5)}/>
+          </div>
+          <div className='row'>
+            <Square value={table[6]} onclick={() => onClickSquare(6)}/>
+            <Square value={table[7]} onclick={() => onClickSquare(7)}/>
+            <Square value={table[8]} onclick={() => onClickSquare(8)}/>
+          </div>
         </div>
-        <div className='row'>
-          <Square value={table[3]} onclick={() => onClickSquare(3)}/>
-          <Square value={table[4]} onclick={() => onClickSquare(4)}/>
-          <Square value={table[5]} onclick={() => onClickSquare(5)}/>
-        </div>
-        <div className='row'>
-          <Square value={table[6]} onclick={() => onClickSquare(6)}/>
-          <Square value={table[7]} onclick={() => onClickSquare(7)}/>
-          <Square value={table[8]} onclick={() => onClickSquare(8)}/>
-        </div>
-      </div>
-    </div>
+      }
+    </Grid>
   );
 }
 
